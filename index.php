60f5294b3b7963fe52f9afb65bf1dc5105d3d734
Viszt Péter <info@visztpeter.me>
2020-02-23 19:23:24 +0100

    v2.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: WooCommerce DPD Weblabel Export
 Plugin URI: http://visztpeter.me
 Description: Rendelésinfó exportálása DPD Weblabel importáláshoz
 Author: Viszt Péter
-Version: 1.0.3
+Version: 2.0
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -17,15 +17,15 @@ class WC_DPD_Weblabel {
 	public static $plugin_basename;
 	public static $version;
 
-    //Construct
-    public function __construct() {
+	//Construct
+	public function __construct() {
 
 		//Default variables
 		self::$plugin_prefix = 'wc_dpd_weblabel_';
 		self::$plugin_basename = plugin_basename(__FILE__);
 		self::$plugin_url = plugin_dir_url(self::$plugin_basename);
 		self::$plugin_path = trailingslashit(dirname(__FILE__));
-		self::$version = '1.0.3';
+		self::$version = '2.0';
 
 		add_action( 'admin_init', array( $this, 'wc_dpd_weblabel_admin_init' ) );
 		add_filter( 'woocommerce_shipping_settings', array( $this, 'settings' ) );
@@ -34,13 +34,13 @@ class WC_DPD_Weblabel {
  		add_action( 'restrict_manage_posts', array( $this, 'restrict_manage_posts' ) );
  		add_action( 'woocommerce_order_actions_start', array( $this, 'single_order_button' ) );
 
-    }
+  }
 
-    //Add CSS & JS
+	//Add CSS & JS
 	public function wc_dpd_weblabel_admin_init() {
-        wp_enqueue_style( 'wc_dpd_css', plugins_url( '/global.css',__FILE__ ) );
-				wp_enqueue_script( 'wc_dpd_js', plugins_url( '/global.js',__FILE__ ) );
-    }
+		wp_enqueue_style( 'wc_dpd_css', plugins_url( '/global.css',__FILE__ ) );
+		wp_enqueue_script( 'wc_dpd_js', plugins_url( '/global.js',__FILE__ ) );
+	}
 
 	//Settings
 	public function settings( $settings ) {
@@ -72,18 +72,19 @@ class WC_DPD_Weblabel {
 
 	//Order list button
 	public function order_row_button($order) {
+		$order_id = $order->get_id();
 		?>
-		<a href="<?php echo admin_url( "?download_dpd_csv=1&order_id=$order->id" ); ?>" class="button tips dpd-small-button" target="_blank" alt="" data-tip="<?php _e('DPD Weblabel Export','wc-szamlazz'); ?>">
-			<img src="<?php echo WC_DPD_Weblabel::$plugin_url . 'dpd.png'; ?>" alt="" width="16" height="16">
+		<a href="<?php echo admin_url( "?download_dpd_csv=1&order_id=$order_id" ); ?>" class="button tips dpd-small-button" target="_blank" alt="" data-tip="<?php _e('DPD Weblabel Export','wc-szamlazz'); ?>">
+			<img src="<?php echo WC_DPD_Weblabel::$plugin_url . 'dpd-logo.svg'; ?>" alt="" width="16" height="20">
 		</a>
 		<?php
 	}
 
 	//Single order button
-	public function single_order_button($order) {
+	public function single_order_button($order_id) {
 		?>
 		<li class="wide dpd-big-single-button">
-			<label><?php _e('DPD Weblabel','wc-szamlazz'); ?></label> <a href="<?php echo admin_url( "?download_dpd_csv=1&order_id=$order" ); ?>" class="button" target="_blank" alt="" data-tip="<?php _e('DPD Weblabel Export','wc-szamlazz'); ?>"><?php _e('Letöltés','wc-szamlazz'); ?></a>
+			<label><?php _e('DPD Weblabel','wc-szamlazz'); ?></label> <a href="<?php echo admin_url( "?download_dpd_csv=1&order_id=$order_id" ); ?>" class="button" target="_blank" alt="" data-tip="<?php _e('DPD Weblabel Export','wc-szamlazz'); ?>"><?php _e('Letöltés','wc-szamlazz'); ?></a>
 		</li>
 		<?php
 	}
@@ -94,7 +95,7 @@ class WC_DPD_Weblabel {
 
 		if ( $typenow == 'shop_order' ) {
 			?>
-			<a href="<?php echo admin_url( "?download_dpd_csv=1&order_id=$order->id" ); ?>" data-baseurl="<?php echo admin_url( "?download_dpd_csv=1&order_id=$order->id" ); ?>" class="button dpd-big-button" id="dpd-export-button" target="_blank" alt="" data-tip="<?php _e('DPD Weblabel Export','wc-szamlazz'); ?>"><?php _e('DPD Weblabel Export','wc-szamlazz'); ?><small></small></a>
+			<a href="<?php echo admin_url( "?download_dpd_csv=1&order_id=" ); ?>" data-baseurl="<?php echo admin_url( "?download_dpd_csv=1&order_id=" ); ?>" class="button dpd-big-button" id="dpd-export-button" target="_blank" alt="" data-tip="<?php _e('DPD Weblabel Export','wc-szamlazz'); ?>"><?php _e('DPD Weblabel Export','wc-szamlazz'); ?><small></small></a>
 			<?php
 		}
 	}
@@ -108,10 +109,9 @@ class WC_DPD_Weblabel {
 			}
 
 			//Set file name
-			if(isset($_GET['order_id'])) {
-				if($_GET['order_id'] != '') {
-					$filename = 'dpd'.date('-Y-m-d-H:i').'-'.$_GET['order_id'].'.csv';
-				}
+			if(isset($_GET['order_id']) && $_GET['order_id'] != '') {
+				$order_id = sanitize_text_field($_GET['order_id']);
+				$filename = 'dpd'.date('-Y-m-d-H:i').'-'.$order_id.'.csv';
 			} else {
 				$filename = 'dpd'.date('-Y-m-d-H:i').'.csv';
 			}
@@ -130,92 +130,102 @@ class WC_DPD_Weblabel {
 				'posts_per_page' => '-1'
 			);
 
-			if(isset($_GET['order_id'])) {
-				if($_GET['order_id'] != '') {
-					$orderids = explode('|',$_GET['order_id']);
-					$args['post__in'] = $orderids;
-				}
+			if(isset($_GET['order_id']) && $_GET['order_id'] != '') {
+				$orderids = array_map('intval', explode(',', $_GET['order_id']));
 			} else {
 
-				//2.2 előtt taxonomy volt a rendelés státusz
-				global $woocommerce;
-				if($woocommerce->version<2.2) {
-					$args['tax_query'] = array(
-						array(
-							'taxonomy' => 'shop_order_status',
-							'field' => 'slug',
-							'terms' => array('processing')
-						)
-					);
-				} else {
-					$args['post_status'] =  array( 'wc-processing');
-				}
+				//Get all processing orders
+				$orderids = wc_get_orders( array(
+					'status' => 'processing',
+					'return' => 'ids',
+				));
 
 			}
 
-			$orders = new WP_Query($args);
-
+			//Convert WooCommerce country codes to DPD versions
 			$dpd_country_codes = array('AT' => 'A', 'BA' => 'BIH', 'DE' => 'D', 'ES' => 'E', 'FI' => 'FIN', 'FR' => 'F', 'HU' => 'H', 'IE' => 'IRL', 'IT' => 'I', 'LU' => 'L', 'NO' => 'N', 'PT' => 'P', 'RU' => 'GUS', 'SE' => 'S', 'SI' => 'SLO');
 
+			//Check the cod payment method
+			$cash_payment_method = get_option( 'wc_dpd_cash_payment_gateway', 'none' );
+
+			//Generate output
 			$output = fopen("php://output", "w");
-			while ( $orders->have_posts() ) : $orders->the_post();
-				global $post;
-				$order_id = get_the_ID();
-				$order = new WC_Order($order_id);
 
-				$order_type = 'D';
+			foreach ($orderids as $order_id) {
+				$order = wc_get_order($order_id);
 
-				$cash_payment_method = get_option( 'wc_dpd_cash_payment_gateway', 'none' );
+				//If COD
+				$is_cod = ($order->get_payment_method() == $cash_payment_method);
 
-				//Ha utánvétes payu fizetés
-				if($order->payment_method == $cash_payment_method) {
-					$order_type = 'D-COD';
-				}
+				//Set default type
+				$order_type = ($is_cod) ? 'D-COD' : 'D';
 
 				//Súly
 				$weight = 0;
-				foreach( $order->get_items() as $item ) {
-					$_product = new WC_Product($item['product_id']);
-					$weight += $item['qty']*$_product->get_weight();
+				foreach( $order->get_items() as $order_item ) {
+					$product = $order_item->get_product();
+					if($product && $product->get_weight()) {
+						$weight += $order_item->get_quantity()*$product->get_weight();
+					}
 				}
+
+				/*
+				A oszlop: Példák: „D” / „D-DOCRET” / „D-COD” / „D-CODEX” / „D-COD-DOCRET” / - Normál / Normál Szállítólevél viszaforgatással /Utánvétes csomag / Utánvételes csomag express / Utánvétes csomag szállítólevél visszaforgatással , minden esetben ennek kell lennie, ezekkel kell kezdődnie - kötelező / kötelező
+				B oszlop: 0.6 - Csomag súlya: elhagyható parcel weight: optional
+				C oszlop: "" / 111 - Normál csomag esetén nincs értelmezve / COD érték - elhagyható / kötelező COD value, used only for COD parcels
+				D oszlop: ”” / szla:111 - Normál csomag esetén nincs értelmezve / Beszedés indítéka (pl.: számlaszám) - elhagyható / kötelező COD reference optional
+				E oszlop: Referencia - Önök referencia (azonosító) száma, például számlaszám - elhagyható Parcel reference, optional
+				F oszlop: Címtörzs id - Megrendelő címnyilvántartási száma - elhagyható Address reference: optional G oszlop: Minta cegnév – címzett neve (vagy magánszemélynél a magánszemély neve) – Kötelező Address name Mandatory
+				H oszlop: Minta nev 2 - Céges környezetben a címzett neve . elhagyható Address name2 mandatory
+				I oszlop: Minta utca 1 - A cél címe - kötelező Address1: mandatory
+				J oszlop: Minta utca 2 - A cél címe - elhagyható Address2: mandatory
+				K oszlop: H - Ország betűkód – Kötelező Country code: mandatory
+				L oszlop: 1158 - Irányítószám – Kötelező Postal code: mandatory
+				M oszlop: Budapest - Város – Kötelező City: mandatory
+				N oszlop: 1234567 - Telefonszám (futár tudja értesíteni a címzettet) - elhagyható Phone number: optional
+				O oszlop: 7654321 - Fax szám – elhagyható Fax number: optional
+				P oszlop: valaki@valahol.hu - E-mail cím, ennek megléte esetén a csomag címzettje e-mailt kap a csomag felvétele (a mi telephelyünkön!) után – elhagyható Email address: optional
+				Q oszlop: Megjegyzés a céghez tárolható megjegyzés – elhagyható Remark for delivery:optional
+				R oszlop: IDM SMS telefonszám - elhagyható IDM SMS Number: optional
+				*/
 
 				$csv_row = array();
 				$csv_row[0] = $order_type; //Csomag típusa, D normál, D-COD utánvétes
 				$csv_row[1] = $weight; //Súly
 				$csv_row[2] = ''; //Utánvét összeg
-					if($order->payment_method == $cash_payment_method) { $csv_row[2] = $order->order_total; }
+					if($is_cod) { $csv_row[2] = $order->get_total(); }
 				$csv_row[3] = ''; //Utánvét ref.
-					if($order->payment_method == $cash_payment_method) { $csv_row[3] = $order->get_order_number(); }
+					if($is_cod) { $csv_row[3] = $order->get_order_number(); }
 				$csv_row[4] = $order->get_order_number(); //Referencia szám(rendelés szám)
 				$csv_row[5] = ''; //Címtörzs ID(???)
 
 				//Ha cégnév van, az az elsődleges, különben csak a nevet küldjük
-				if($order->shipping_company) {
-					$csv_row[6] = $order->shipping_company;
-					$csv_row[7] = $order->shipping_first_name.' '.$order->shipping_last_name; //Név
+				if($order->get_shipping_company()) {
+					$csv_row[6] = $order->get_shipping_company();
+					$csv_row[7] = $order->get_formatted_shipping_full_name(); //Név
 				} else {
-					$csv_row[6] = $order->shipping_first_name.' '.$order->shipping_last_name;
+					$csv_row[6] = $order->get_formatted_shipping_full_name();
 					$csv_row[7] = ''; //Név
 				}
-				$csv_row[8] = $order->shipping_address_1; //Cím 1
-				$csv_row[9] = $order->shipping_address_2; //Cím 2
-				if(array_key_exists($order->shipping_country, $dpd_country_codes)) {
-					$csv_row[10] = $dpd_country_codes[$order->shipping_country]; //Országkód
+				$csv_row[8] = $order->get_shipping_address_1(); //Cím 1
+				$csv_row[9] = $order->get_shipping_address_2(); //Cím 2
+				if(array_key_exists($order->get_shipping_country(), $dpd_country_codes)) {
+					$csv_row[10] = $dpd_country_codes[$order->get_shipping_country()]; //Országkód
 				} else {
-					$csv_row[10] = $order->shipping_country; //Országkód
+					$csv_row[10] = $order->get_shipping_country(); //Országkód
 				}
-				$csv_row[11] = $order->shipping_postcode; //Irányítószám
-				$csv_row[12] = $order->shipping_city; //Város
-				$csv_row[13] = $order->billing_phone; //Telefon
+				$csv_row[11] = $order->get_shipping_postcode(); //Irányítószám
+				$csv_row[12] = $order->get_shipping_city(); //Város
+				$csv_row[13] = $order->get_billing_phone(); //Telefon
 				$csv_row[14] = ''; //Fax
-				$csv_row[15] = $order->billing_email; //Email
+				$csv_row[15] = $order->get_billing_email(); //Email
 				$csv_row[16] = ''; //Céggel kapcsolatos megjegyzés
 
-				$csv_row = apply_filters('wc_dpd_weblabel_item',$csv_row,$order_id);
+				$csv_row = apply_filters('wc_dpd_weblabel_item', $csv_row, $order_id);
 
 				fputcsv($output, $csv_row, ';');
 
-			endwhile;
+			};
 			fclose($output);
 
 			exit();
@@ -224,6 +234,6 @@ class WC_DPD_Weblabel {
 	}
 }
 
-$GLOBALS['wc_dpd_weblabel'] = new WC_DPD_Weblabel();
+$wc_dpd_weblabel = new WC_DPD_Weblabel();
 
 ?>
